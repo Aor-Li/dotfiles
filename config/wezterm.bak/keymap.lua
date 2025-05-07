0a61226619fcde852ca39e6ddd276d057b232303
aor <liyifeng0039@gmail.com>
2025-05-08 01:24:11 +0800

    remove

--- config/wezterm.bak/keymap.lua
+++
@@ -1,6 +0,0 @@
-local wezterm = require 'wezterm'
-local module = {}
-function module.apply_to_config(config)
-    -- I have no keymap setting currently
-end
-return module
--- config/wezterm.bak/wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
-
-require('appearance').apply_to_config(config)
-require('function').apply_to_config(config)
-require('keymap').apply_to_config(config)
-
-return config
