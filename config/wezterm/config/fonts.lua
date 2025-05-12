5d362ee1af938f1f7e1b550012f8882e8237dcdf
aor <liyifeng0039@gmail.com>
2025-05-13 00:20:29 +0800

    reformat wezterm config

---
+++ config/wezterm/config/fonts.lua
@@ -0,0 +1,15 @@
+local wezterm = require('wezterm')
+local platform = require('utils.platform')
+
+return {
+  font = wezterm.font({
+    family = 'ComicShannsMono Nerd Font',
+    weight = 'Medium',
+    harfbuzz_features = { 'calt', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09', 'liga' },
+  }),
+  font_size = 12,
+
+  --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
+  freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+  freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+}
